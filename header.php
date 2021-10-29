b8bb076912964908fd0993552a37b81343511db3
Aaron Thunnissen <aaronthunnissen@live.nl>
2021-10-29 17:22:06 +0200

    changed HTML to PHP

---
+++ header.php
@@ -0,0 +1,22 @@
+
+<!DOCTYPE html>
+<html lang='nl'>
+<head>
+<meta charset='UTF-8'/>
+<meta name="description" 
+content="Aaron Thunnissen, Software Development Student at HAN University of Applied Sciences"/>
+<title>Aaron Thunnissen</title>
+<link href='css/styles.css' rel='stylesheet'/>
+<link href='css/menu.css' rel='stylesheet'/>
+</head>
+<body>
+<header>
+<nav>
+<ul class='menu'>
+<li><a href='index.php'>Home</a></li>
+</ul>
+</nav>
+</header>
+
+
+
